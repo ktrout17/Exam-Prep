a867b5f45c8bc54ccb6c91e31015ff9428c03ae8
ktrout17 <ktrout@student.wethinkcode.co.za>
2019-08-14 08:21:09 +0200

    tested

--- 04/ft_split/ft_split.c
+++ 04/ft_split/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ktrout <ktrout@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/05 07:04:41 by ktrout            #+#    #+#             */
-/*   Updated: 2019/08/13 13:04:54 by ktrout           ###   ########.fr       */
+/*   Updated: 2019/08/14 08:20:37 by ktrout           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 
 char		**ft_split(char *str)
 {
-	int		i;
-	int		j;
-	int		k;
-	char	**split;
+	int i;
+	int j;
+	int k;
+	char **split;
 
 	i = 0;
 	k = 0;
 	if (!(split = (char **)malloc(sizeof(char *) * 256)))
 		return (NULL);
 	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-		i += 1;
+		i++;
 	while (str[i])
 	{
 		j = 0;
@@ -34,17 +34,29 @@ char		**ft_split(char *str)
 		while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i])
 			split[k][j++] = str[i++];
 		while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-			i += 1;
+			i++;
 		split[k][j] = '\0';
-		k += 1;
+		k++;
 	}
 	split[k] = NULL;
 	return (split);
 }
 
-int		main(void)
+int		main(int argc, char **argv)
 {
-	char str[] = "Hello, my name is Kristin";
-	printf("%s", *ft_split(str));
+	int i;
+	char **str;
+
+	if (argc == 2)
+	{
+		if (argv[1])
+		{
+			i = 0;
+			str = ft_split(argv[1]);
+			while (str[i])
+				printf("%s\n", str[i++]);
+		}
+	}
+	printf("\n");
 	return (0);
 }
